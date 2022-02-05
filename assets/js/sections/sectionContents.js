710a889fafa0507b47f40d4ca170deb1b3ab5004
alade-01 <aladewilliame1998@gmail.com>
2022-02-05 12:30:37 +0000

    Update accordion skills.

--- assets/js/sections/sectionContents.js
+++ assets/js/sections/sectionContents.js
@@ -35,7 +35,32 @@ skillsHeader.forEach((el) =>{
 
  */
 
+/*==================== ACCORDION SKILLS UPDATE====================*/
 
+// toggle answer
+
+const toggleAnswer = (e) => {
+    const question = e.childNodes[1];
+    hideAllAnswers(question);
+    e.childNodes[5].classList.toggle("rotate-icon");
+    // e.classList.toggle("bold");
+    e.nextElementSibling.classList.toggle("hide");
+    //e.nextElementSibling.classList.add("transitionOpen");
+};
+
+//hide answers
+
+const hideAllAnswers = (question) => {
+    const questionsEl = [...document.querySelectorAll(".skills__header")];
+    questionsEl.forEach((questionEl) => {
+        console.log();
+        if (questionEl.childNodes[1] !== question) {
+            questionEl.classList.add("transitionOpen");
+            questionEl.nextElementSibling.classList.add("hide");
+            questionEl.childNodes[5].classList.remove("rotate-icon");
+        }
+    });
+};
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),

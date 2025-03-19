eb8c491a10d7d820f020f7ce6c455a86e829bfcb
alade-01 <aladewilliame1998@gmail.com>
2025-03-19 01:33:12 -0400

    update according skills file

--- res/js/accordionSkills.js
+++ res/js/accordionSkills.js
@@ -23,3 +23,45 @@ const hideAllAnswers = (question) => {
         }
     });
 };
+
+/**
+ * /*==================== ACCORDION SKILLS UPDATE ====================*
+
+// Toggle answer visibility and rotate icon
+const toggleAnswer = (e) => {
+    const questionHeader = e.querySelector('.skills__header');
+    const answer = e.nextElementSibling;
+    const icon = e.querySelector('.rotate-icon');
+    
+    // Hide all answers except the clicked one
+    hideAllAnswers(questionHeader);
+
+    // Toggle the answer visibility and rotate the icon
+    icon.classList.toggle('rotate-icon');
+    answer.classList.toggle('hide');
+};
+
+// Hide all answers and reset icons
+const hideAllAnswers = (currentQuestion) => {
+    const questions = document.querySelectorAll('.skills__header');
+
+    questions.forEach((question) => {
+        // Only hide the answers for questions that are not the current one
+        if (question !== currentQuestion) {
+            const icon = question.querySelector('.rotate-icon');
+            const answer = question.nextElementSibling;
+
+            // Reset all other questions
+            icon.classList.remove('rotate-icon');
+            answer.classList.add('hide');
+        }
+    });
+};
+
+// Add event listeners for each accordion item
+const accordionItems = document.querySelectorAll('.accordion-item');
+accordionItems.forEach(item => {
+    item.addEventListener('click', (e) => toggleAnswer(e.target));
+});
+
+ */
